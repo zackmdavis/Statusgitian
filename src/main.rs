acb158b2dd981249a965072e8d8a3ec5605f1ca9
Zack M. Davis <code@zackmdavis.net>
2018-05-05 16:14:50 -0700

    find commits with biggest author/committer time gaps

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,17 @@ extern crate prettytable;
 
 use std::env;
 use std::cmp::Ordering;
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 use std::error::Error;
 
-use git2::{Commit, Repository};
+use git2::{Commit, Oid, Repository};
 use prettytable::Table;
 use prettytable::format;
+
+// TODO: trait that generalizes the wordcount and author–commit-gap state
+// structures; any given revwalk on a repository can take an arbitrary vector
+// of these trait objects and call `register_commit` on all of them for each
+// commit
 
 #[derive(Debug, Default)]
 struct Counts {
@@ -23,7 +28,7 @@ impl Counts {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct Counter(HashMap<String, Counts>);
 
 impl Counter {
@@ -42,21 +47,41 @@ impl Counter {
     }
 }
 
-fn wordcount_repo(path: &str) -> Result<Counter, Box<Error>> {
-    let mut counter = Counter(HashMap::new());
+#[derive(Debug, Default)]
+struct AuthorCommitterTimeGapFinder(BTreeMap<i64, Oid>);
+
+impl AuthorCommitterTimeGapFinder {
+    fn register_commit(&mut self, commit: &Commit) -> Result<(), Box<Error>> {
+        let authorship_timestamp = commit.author().when().seconds();
+        let commit_timestamp = commit.committer().when().seconds();
+        let gap = commit_timestamp - authorship_timestamp;
+        if gap != 0 {
+            self.0.insert(gap, commit.id());
+        }
+        Ok(())
+    }
+}
+
+fn analyze_repo(
+    path: &str,
+) -> Result<(Counter, AuthorCommitterTimeGapFinder), Box<Error>> {
+    let mut counter = Counter::default();
+    let mut gapfinder = AuthorCommitterTimeGapFinder::default();
     let repo = Repository::open(path)?;
     let mut revwalk = repo.revwalk()?;
     revwalk.push_head()?;
     for oid in revwalk {
         let commit = repo.find_commit(oid?)?;
         counter.register_commit(&commit)?;
+        gapfinder.register_commit(&commit)?;
     }
-    Ok(counter)
+    Ok((counter, gapfinder))
 }
 
 fn main() {
     let args = env::args().collect::<Vec<_>>();
-    let counter = wordcount_repo(&args[1]).expect("couldn't count");
+    let (counter, gapfinder) =
+        analyze_repo(&args[1]).expect("couldn't analyze");
     let mut roster = counter
         .0
         .iter()
@@ -88,4 +113,7 @@ fn main() {
         ]);
     }
     table.printstd();
+
+    // TODO: pretty table
+    println!("{:?}", gapfinder);
 }
