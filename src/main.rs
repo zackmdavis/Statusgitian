dce4a75528ed5cf8b4c7846b09fcf842b6e86fce
Zack M. Davis <code@zackmdavis.net>
2018-05-05 19:34:08 -0700

    trait!

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,10 @@ use git2::{Commit, Oid, Repository};
 use prettytable::Table;
 use prettytable::format;
 
-// TODO: trait that generalizes the wordcount and author–commit-gap state
-// structures; any given revwalk on a repository can take an arbitrary vector
-// of these trait objects and call `register_commit` on all of them for each
-// commit
+trait Analyzer {
+    fn register_commit(&mut self, commit: &Commit) -> Result<(), Box<Error>>;
+    fn report(&self);
+}
 
 #[derive(Debug, Default)]
 struct Counts {
@@ -31,7 +31,7 @@ impl Counts {
 #[derive(Debug, Default)]
 struct Counter(HashMap<String, Counts>);
 
-impl Counter {
+impl Analyzer for Counter {
     fn register_commit(&mut self, commit: &Commit) -> Result<(), Box<Error>> {
         let author =
             commit.author().name().ok_or("malformed name")?.to_owned();
@@ -45,75 +45,86 @@ impl Counter {
         counts.words += wordcount;
         Ok(())
     }
+
+    fn report(&self) {
+        let mut roster = self.0
+            .iter()
+            .map(|(name, counts)| {
+                (
+                    name,
+                    counts.commits,
+                    counts.words,
+                    counts.words_per_commit(),
+                )
+            })
+            .collect::<Vec<_>>();
+        roster.sort_by(|a, b| b.3.partial_cmp(&a.3).unwrap_or(Ordering::Less));
+
+        let mut table = Table::new();
+        table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
+        table.set_titles(row![
+            "author",
+            "total commits",
+            "total words",
+            "words / commit"
+        ]);
+        for (name, total_commits, total_words, words_per_commit) in roster {
+            table.add_row(row![
+                name,
+                total_commits,
+                total_words,
+                words_per_commit
+            ]);
+        }
+        table.printstd();
+    }
 }
 
 #[derive(Debug, Default)]
 struct AuthorCommitterTimeGapFinder(BTreeMap<i64, Oid>);
 
-impl AuthorCommitterTimeGapFinder {
+impl Analyzer for AuthorCommitterTimeGapFinder {
     fn register_commit(&mut self, commit: &Commit) -> Result<(), Box<Error>> {
         let authorship_timestamp = commit.author().when().seconds();
         let commit_timestamp = commit.committer().when().seconds();
         let gap = commit_timestamp - authorship_timestamp;
         if gap != 0 {
+            // XXX: this overwrites duplicate gaps
             self.0.insert(gap, commit.id());
         }
         Ok(())
+    }
+
+    fn report(&self) {
+        // TODO: pretty table
+        println!("{:?}", self);
     }
 }
 
 fn analyze_repo(
     path: &str,
-) -> Result<(Counter, AuthorCommitterTimeGapFinder), Box<Error>> {
-    let mut counter = Counter::default();
-    let mut gapfinder = AuthorCommitterTimeGapFinder::default();
+    analyzers: &mut [Box<Analyzer>],
+) -> Result<(), Box<Error>> {
     let repo = Repository::open(path)?;
     let mut revwalk = repo.revwalk()?;
     revwalk.push_head()?;
     for oid in revwalk {
         let commit = repo.find_commit(oid?)?;
-        counter.register_commit(&commit)?;
-        gapfinder.register_commit(&commit)?;
+        for analyzer in analyzers.iter_mut() {
+            analyzer.register_commit(&commit)?;
+        }
     }
-    Ok((counter, gapfinder))
+    Ok(())
 }
 
 fn main() {
     let args = env::args().collect::<Vec<_>>();
-    let (counter, gapfinder) =
-        analyze_repo(&args[1]).expect("couldn't analyze");
-    let mut roster = counter
-        .0
-        .iter()
-        .map(|(name, counts)| {
-            (
-                name,
-                counts.commits,
-                counts.words,
-                counts.words_per_commit(),
-            )
-        })
-        .collect::<Vec<_>>();
-    roster.sort_by(|a, b| b.3.partial_cmp(&a.3).unwrap_or(Ordering::Less));
-
-    let mut table = Table::new();
-    table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
-    table.set_titles(row![
-        "author",
-        "total commits",
-        "total words",
-        "words / commit"
-    ]);
-    for (name, total_commits, total_words, words_per_commit) in roster {
-        table.add_row(row![
-            name,
-            total_commits,
-            total_words,
-            words_per_commit
-        ]);
+    let mut analyzers: Vec<Box<Analyzer>> = vec![
+        Box::new(Counter::default()),
+        Box::new(AuthorCommitterTimeGapFinder::default()),
+    ];
+    analyze_repo(&args[1], &mut analyzers).expect("couldn't analyze");
+    for analyzer in analyzers {
+        analyzer.report();
     }
-    table.printstd();
-
-    // TODO: pretty table
-    println!("{:?}", gapfinder);
 }
